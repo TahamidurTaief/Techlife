36d3c2cdd798c898406f09608f01fed6c8e6c09d
TahamidurTaief <tahamidurtaief@gmail.com>
2026-04-11 13:51:14 +0600

    update image and css config and gitignore

--- staticfiles/js/print_function.js
+++
@@ -1,7 +0,0 @@
-function printBlogContent() {
-    const contentToPrint = document.getElementById('blog-content-to-print').innerHTML;
-    const originalContent = document.body.innerHTML;
-    document.body.innerHTML = contentToPrint;
-    window.print();
-    document.body.innerHTML = originalContent;
-}
--- staticfiles/js/success_message_for_create.js
+++
@@ -1,9 +0,0 @@
-document.body.addEventListener('htmx:afterOnLoad', function(evt) {
-    const toasts = document.querySelectorAll('.toast-message');
-    toasts.forEach(toast => {
-        setTimeout(() => {
-            toast.style.opacity = '0';
-            setTimeout(() => toast.remove(), 500);
-        }, 3000); // 3 seconds
-    });
-});
